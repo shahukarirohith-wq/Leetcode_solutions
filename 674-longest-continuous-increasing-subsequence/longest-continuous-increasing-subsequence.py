2d848f0fd1023ead7f21dbdbc6d0c96f0a77c2d5
Shahukari Rohith <shahukarirohith@gmail.com>
2026-09-09 09:47:34 +0530

    Time: 3 ms (53.82%) | Memory: 20.3 MB (97.95%) - LeetSync

---
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        m=1
+        l=0
+        r=1
+        c=1
+        while r<len(nums):
+            if nums[l]<nums[r]:
+                c+=1
+                m=max(c,m)
+                r+=1
+                l+=1
+            else:
+                l+=1
+                r+=1
+                c=1
+        return m
